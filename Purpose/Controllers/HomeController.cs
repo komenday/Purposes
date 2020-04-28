8ed0d7b98e049ae7dcff551ee486b1742d3fbaff
yurii komenda <komenday@gmail.com>
2020-04-28 18:45:04 +0300

    Ready to work
    Database connection, data context configured. Master model created.

--- Purpose/Controllers/HomeController.cs
+++ Purpose/Controllers/HomeController.cs
@@ -11,27 +11,15 @@ namespace Purpose.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private PurposeContext db;
+        public HomeController(PurposeContext context)
         {
-            _logger = logger;
+            db = context;
         }
 
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Purpose/Startup.cs
+++ Purpose/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Purpose.Models;
 
 namespace Purpose
 {
@@ -23,6 +25,8 @@ namespace Purpose
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<PurposeContext>(options => options.UseSqlServer(connection));
             services.AddControllersWithViews();
         }
 
